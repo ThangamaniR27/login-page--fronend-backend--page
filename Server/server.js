229ca8cf26d65e3558c0eada160f89788a82fc8a
ThangamaniR <thangamanim1727@email.com>
2026-05-18 10:40:25 +0530

    updated login project

--- Server/server.js
+++ Server/server.js
@@ -3,25 +3,29 @@ const cors = require("cors");
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
-
 
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
-  
-  console.log("Received:", email, password);
+  console.log(email, password);
 
-  
-  if (email === "test@gmail.com" && password === "1234") {
-    res.json({ message: "Login successful" });
-  } else {
-    res.status(401).json({ message: "Invalid credentials" });
+  if (
+    email === "admin@gmail.com" &&
+    password === "12345"
+  ) {
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+    });
   }
-});
 
+  return res.status(401).json({
+    success: false,
+    message: "Invalid credentials",
+  });
+});
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
