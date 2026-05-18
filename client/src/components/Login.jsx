d008533bc0311f8643011dd5cda1acdb5f2bda3c
ThangamaniR <thangamanim1727@email.com>
2026-05-18 16:57:21 +0530

    Fix client folder (remove submodule)

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    if (!email || !password) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    try {
+      const res = await axios.post("http://localhost:5000/login", {
+        email,
+        password,
+      });
+
+      if (res.data.success) {
+        alert("Login Successful");
+        navigate("/dashboard");
+      } else {
+        alert("Invalid credentials");
+      }
+
+    } catch (err) {
+  console.log(err);
+
+  if (err.response) {
+    alert(err.response.data.message);
+  } else {
+    alert("Backend server not running");
+  }
+}
+  };
+
+  return (
+    <div className="container">
+      <div className="login-box">
+        <h1 className="logo">Spotify</h1>
+        <p className="subtext">Log in to your account</p>
+
+        <input
+          type="email"
+          placeholder="Email address"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button onClick={handleLogin}>Log In</button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
